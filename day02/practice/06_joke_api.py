38dd5e19648687d0b7b4a08429a6580991d8b1e7
root <root@DESKTOP-50HQQIN.localdomain>
2026-03-21 10:41:36 +0000

    latest commit

--- day02/practice/06_joke_api.py
+++ day02/practice/06_joke_api.py
@@ -5,4 +5,7 @@ def get_jokes():
     joke=requests.get(url=api_url)
     final_joke = joke.json()["setup"] + joke.json()["punchline"]
     return final_joke
-get_jokes()
+
+final_joke= get_jokes()
+
+print (final_joke)
